6c5c20bcb92d2837f0398d9192fde669de913d90
Cauê Mustafá <cauemustafa@live.com>
2023-03-30 17:00:33 -0300

    feat: video component

---
+++ src/pages/Video.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from 'react';
+
+import './video.css';
+
+function Video() {
+  const [play, setPlay] = useState(false);
+
+  const videoRef = useRef(null);
+
+  const handleStart = () => {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  };
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/brecker2.mp4?alt=media&token=5493f40a-3b0d-4c19-b5dd-a9016cc5a6d1"
+      />
+    </div>
+  );
+}
+
+export default Video;
