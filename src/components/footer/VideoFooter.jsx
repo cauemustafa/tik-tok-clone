1043b4fc4c74511ffa7b6b1c7bb6ece70b23251f
Cauê Mustafá <cauemustafa@live.com>
2023-04-04 02:08:23 -0300

    style: fix position of video footer description

--- src/components/footer/VideoFooter.jsx
+++ src/components/footer/VideoFooter.jsx
@@ -3,12 +3,12 @@ import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 import './videoFooter.css';
 
-function VideoFooter({username, description, music}) {
+function VideoFooter({ username, description, music }) {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
         <h3>@{username}</h3>
-        <p>{description}</p>
+        <p className="videoFooter__description">{description}</p>
         <div className="videoFooter__music">
           <MusicNoteIcon />
           <div className="videoFooterMusic__text">
@@ -18,7 +18,7 @@ function VideoFooter({username, description, music}) {
       </div>
       <img
         className="videoFooter__record"
-        src='https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/vinil.png?alt=media&token=72a6362d-ca03-4b8b-975e-a4832fdeccff'
+        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/vinil.png?alt=media&token=72a6362d-ca03-4b8b-975e-a4832fdeccff"
         alt="vinil girando"
       />
     </div>
